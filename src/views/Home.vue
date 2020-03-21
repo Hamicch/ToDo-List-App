aa0d1bff0fc875178d2473c0188f25aec47d73b6
Hamicch <47416177+Hamicch@users.noreply.github.com>
2020-03-21 11:47:14 +0100

    a minor change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -48,7 +48,7 @@ export default {
 
       },
       created() {
-        axios.get('https://jsonplaceholder.typicode.com/todos?_limit')
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=1')
                 .then(res => this.todos = res. data)
                 .catch(err => console.log(err));
     }
